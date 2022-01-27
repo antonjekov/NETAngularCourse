9c7caf1f0d621281a28264ead0820dd49f84cd10
Anton Jekov <antonjekov@gmail.com>
2022-01-27 16:57:47 +0100

    first commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private DataContext context;
+
+        public UsersController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        public IEnumerable<AppUser> Get()
+        {
+            var users =  this.context.Users.ToList();
+            return users;
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public AppUser Get(int id)
+        {
+            var user = this.context.Users.FirstOrDefault(x=>x.Id==id);
+            return user;
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ API/Migrations/20220127130241_UserModelUpdate.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Migrations
+{
+    public partial class UserModelUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MyProperty",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
